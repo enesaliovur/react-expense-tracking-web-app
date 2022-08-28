c0a2f8a94eaf937baef44515d6fc0b03b549fa78
enesaliovur <enesaliovur@gmail.com>
2022-08-28 13:41:25 +0300

    uygulama sisteme yüklendi

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+const Chart = ({ dataPoints }) => {
+  const dataPointValues = dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+  return (
+    <div className="chart">
+      {dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          maxValue={totalMaximum}
+          dataPoint={dataPoint}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+
+const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filteredChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filteredChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList expenses={filteredExpenses} />
+      </Card>
+    </>
+  );
+};
+
+export default Expenses;
